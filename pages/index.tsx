02e91f399bf8719dddaf23cc36de2bcdb59f1444
lancelee2885 <lancelee2885@gmail.com>
2021-10-31 14:14:15 -0700

    display loading and routes in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
- 
+import Link from 'next/link'
+import Loader from '../components/Loader'
 
 export default function Home() {
   return (
     <div>
 
-      {}
+      <Loader show={true} />
+      <Link 
+        prefetch={true}
+        href={{
+          pathname: '/[username]',
+          query: {username: 'lancelee'}
+        }}>
+        Lance's Profile</Link>
 
     </div>
   )
