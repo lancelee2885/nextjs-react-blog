953b25746ef8871a3bdbd565de23754ee99e77be
lancelee2885 <lancelee2885@gmail.com>
2021-12-10 18:49:38 -0800

    userform in progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,37 +4,14 @@ import { GetServerSideProps } from "next";
 import Navbar from "../components/Navbar";
 import { Toaster } from "react-hot-toast";
 import UserContext from "../lib/context";
-import { useEffect, useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth, firestore } from "../lib/firebase";
+import { useUserData } from "../lib/hooks";
 
 function MyApp({ Component, pageProps }) {
 
-  const [user] = useAuthState(auth);
-  const [username, setUsername] = useState(null);
-
-  useEffect(() => {
-    // turn off realtime subscription
-    let unsubscribe;
-
-    if (user) {
-      const ref = firestore.collection('users').doc(user.uid);
-
-      // firebase returns a function that when called, will unsubscribe from that data
-      unsubscribe = ref.onSnapshot((doc) => {
-        setUsername(doc.data()?.username);
-      }); 
-    } else {
-      setUsername(null);
-    }
-
-    return unsubscribe;
-
-  }, [user]);
-
+  const userData = useUserData();
 
   return (
-    <UserContext.Provider value={{user, username}}>
+    <UserContext.Provider value={userData}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,23 +1,25 @@
-import { auth, googleAuthProvider } from '../lib/firebase';
-import Image from 'next/image';
-import { useContext } from "react";
+import { auth, firestore, googleAuthProvider } from "../lib/firebase";
+import { useContext, useState, useEffect, useCallback } from "react";
 import UserContext from "../lib/context";
+import debounce from 'lodash.debounce';
 
 export default function Enter(props) {
-
   const { user, username } = useContext(UserContext);
-
 
   // 1. user signed out <SignInButton />
   // 2. user signed in, but missing username <UsernameForm />
   // 3. user signed in, has username <SignOutButton />
   return (
     <main>
-      {user ? 
-        !username ? <UsernameForm /> : <SignOutButton /> 
-        : 
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
         <SignInButton />
-      }
+      )}
     </main>
   );
 }
@@ -30,7 +32,7 @@ function SignInButton() {
 
   return (
     <button className="btn-google" onClick={signInWithGoogle}>
-      <img src='/google.png' alt='google logo' /> Sign in with Google
+      <img src="/google.png" alt="google logo" /> Sign in with Google
     </button>
   );
 }
@@ -41,5 +43,75 @@ function SignOutButton() {
 }
 
 function UsernameForm() {
-  return <>123</>;
+  const [formData, setFormData] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { user, username } = useContext(UserContext);
+
+  useEffect(() => {
+    checkUsername(formData);
+  }, [formData])
+
+  function handleChange(evt) {
+    const val = evt.target.value.toLowerCase();
+    const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+    if (val.length < 3) {
+      setFormData(val);
+      setLoading(false);
+      setIsValid(false);
+    }
+
+    if (re.test(val)) {
+      setFormData(val);
+      setLoading(true);
+      setIsValid(false);
+    }
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+
+  }
+
+  const checkUsername = useCallback(
+    debounce(async (username) => {
+      if (username.length >= 3) {
+        const ref = firestore.doc(`usernames/${username}`);
+        const { exists } = await ref.get();
+        console.log('Firestore read executed!');
+        setIsValid(!exists);
+        setLoading(false);
+      }
+    }, 500),
+    []
+  );
+
+  return (
+    !username && (
+      <section>
+        <h3>Choose Username</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="username"
+            placeholder="username"
+            value={formData}
+            onChange={handleChange}
+          />
+        </form>
+        <button type="submit" className="btn-grenn" disabled={!isValid}>
+          Choose
+        </button>
+        <h3>Debug State</h3>
+        <div>
+          Username: {formData}
+          <br />
+          Loading: {loading.toString()}
+          <br />
+          Username Valid: {isValid.toString()}
+        </div>
+      </section>
+    )
+  );
 }
