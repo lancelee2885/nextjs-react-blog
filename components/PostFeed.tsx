c38c8f61b66ccf9bb80b08e2bb94519fe58ba43e
lancelee2885 <lancelee2885@gmail.com>
2022-01-03 22:01:45 -0800

    PostFeed Page

---
+++ components/PostFeed.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+
+function PostFeed({ posts, admin }) {
+  return posts
+    ? posts.map((post) => (
+        <PostItem post={post} key={post.slug} admin={admin} />
+      ))
+    : null;
+}
+
+export function PostItem({ post, admin }) {
+  const wordCount = post?.content.trim().split(" ").length;
+  const readTime = (wordCount / 100 + 1).toFixed(0);
+
+  return (
+    <div className="card">
+      <Link href={`/${post.username}`}>
+        <a>
+          <strong>By @{post.username}</strong>
+        </a>
+      </Link>
+      <Link href={`/${post.username}/${post.slug}`}>
+        <h2>
+          <a>{post.title}</a>
+        </h2>
+      </Link>
+      <footer>
+        <span>
+          {wordCount} words. {readTime} min read
+        </span>
+        <span className="push-left">💗 {post.heartCount || 0} Hearts</span>
+      </footer>
+
+      {/* If admin view, show extra controls for user */}
+      {admin && (
+        <>
+          <Link href={`/admin/${post.slug}`}>
+            <h3>
+              <button className="btn-blue">Edit</button>
+            </h3>
+          </Link>
+
+          {post.published ? (
+            <p className="text-success">Live</p>
+          ) : (
+            <p className="text-danger">Unpublished</p>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default PostFeed;
