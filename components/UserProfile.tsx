4da876714f0f6965169c55db6a751544a8bf3be1
lancelee2885 <lancelee2885@gmail.com>
2022-01-03 21:51:56 -0800

    user profile page

---
+++ components/UserProfile.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function UserProfile({ user }) {
+  return (
+    <div className="box-center">
+      <img src={user.photoURL} className="card-img-center" />
+      <p>
+        <i>@{user.username}</i>
+      </p>
+      <h1>{user.displayName}</h1>
+    </div>
+  );
+}
+
+export default UserProfile;
