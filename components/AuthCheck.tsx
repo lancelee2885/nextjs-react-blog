41ec9a08c56073cba112b2286e194af9ac7e97bc
lancelee2885 <lancelee2885@gmail.com>
2022-01-23 22:15:48 -0800

    check if user is authenticated

---
+++ components/AuthCheck.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import { useCallback, useContext } from "react";
+import UserContext from "../lib/context";
+
+function AuthCheck(props) {
+    
+    const { username } = useContext(UserContext);
+
+    return username ?
+            props.children :
+            props.fallback || <Link href="/enter">You must be signed in</Link>;
+}
+
+export default AuthCheck
