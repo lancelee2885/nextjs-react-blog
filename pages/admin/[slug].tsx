d24d644f1c81dfca2f7ce63de8ad22278e4808c3
lancelee2885 <lancelee2885@gmail.com>
2022-01-24 22:38:28 -0800

    postform: edit created post

--- pages/admin/[slug].tsx
+++ pages/admin/[slug].tsx
@@ -1,9 +1,99 @@
+import styles from "../../styles/Admin.module.css";
+import { useRouter } from "next/dist/client/router";
+import { useState } from "react";
+import { useDocumentDataOnce } from "react-firebase-hooks/firestore";
+import AuthCheck from "../../components/AuthCheck";
+import { auth, firestore, serverTimestamp } from "../../lib/firebase";
+import { useForm } from "react-hook-form";
+import ReactMarkdown from "react-markdown";
+import toast from "react-hot-toast";
+
 function AdminPostEdit() {
-    return ( 
-        <div>
-            Random Slug!
+  return (
+    <AuthCheck>
+      <PostManager />
+    </AuthCheck>
+  );
+}
+
+// fetch the post from firebase for varies functionalities.
+function PostManager() {
+  const [preview, setPreview] = useState(false);
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const postRef = firestore
+    .collection("users")
+    .doc(auth.currentUser.uid)
+    .collection("posts")
+    .doc(slug);
+
+  const [post] = useDocumentDataOnce(postRef);
+
+  return (
+    <main className={styles.container}>
+      {post && (
+        <>
+          <section>
+            <h1>{post.title}</h1>
+            <p>ID: {post.slug}</p>
+
+            <PostForm
+              postRef={postRef}
+              defaultValues={post}
+              preview={preview}
+            />
+          </section>
+        </>
+      )}
+    </main>
+  );
+}
+
+function PostForm({ postRef, defaultValues, preview }) {
+  const { register, handleSubmit, reset, watch } = useForm({
+    defaultValues,
+    mode: "onChange",
+  });
+
+  async function updatePost({ content, published }) {
+    await postRef.update({
+      content,
+      published,
+      updatedAt: serverTimestamp(),
+    });
+
+    reset({ content, published });
+    toast.success("Post updated successfully.");
+  }
+
+  return (
+    <form onSubmit={handleSubmit(updatePost)}>
+      {preview && (
+        <div className="card">
+          <ReactMarkdown>{watch("content")}</ReactMarkdown>
         </div>
-     );
+      )}
+
+      <div className={preview ? styles.hidden : styles.controls}>
+        <textarea name="content" {...register('content')}></textarea>
+        <fieldset>
+          <label htmlFor="published">Published</label>
+          <input
+            id="published"
+            type="checkbox"
+            className={styles.checkbox}
+            name="published"
+            {...register('published')}
+          />
+        </fieldset>
+
+        <button type="submit" className="btn-greem">
+          Save Changes
+        </button>
+      </div>
+    </form>
+  );
 }
 
 export default AdminPostEdit;
