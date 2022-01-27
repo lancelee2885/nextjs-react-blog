2c5d6b0be4a35a2c5e88fecba1b92eca1a5dc090
lancelee2885 <lancelee2885@gmail.com>
2022-01-26 23:21:29 -0800

    testing on WYSIWYGEditor

--- components/WYSIWYGEditor.tsx
+++ components/WYSIWYGEditor.tsx
@@ -1,21 +1,15 @@
 import React, { useState } from "react";
 
-// Components
 import { EditorState, convertToRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import draftToHtml from "draftjs-to-html";
 
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import "./WYSIWYG.scss";
 
-const WYSIWYGEditor = props => {
+const WYSIWYGEditor = (props) => {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
-  const onEditorStateChange = editorState => {
+  const onEditorStateChange = (editorState) => {
     setEditorState(editorState);
-    console.log("PROPS ==> ", props);
-    return props.onChange(
-      draftToHtml(convertToRaw(editorState.getCurrentContent()))
-    );
   };
 
   return (
