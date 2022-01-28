e04c6109ea7eda89a2fc1cb3da82302912d129e2
lancelee2885 <lancelee2885@gmail.com>
2022-01-27 21:46:10 -0800

    conneted to firebase storage

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -23,7 +23,10 @@ export const firestore = firebase.firestore();
 export const serverTimestamp = firebase.firestore.FieldValue.serverTimestamp;
 
 export const storage = firebase.storage();
+
+// tells the status of file upload
 export const STATE_CHANGED = firebase.storage.TaskEvent.STATE_CHANGED;
+
 export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 /** 
