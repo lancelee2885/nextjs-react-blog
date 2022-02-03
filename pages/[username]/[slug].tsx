7b4105100edf6f92f0fdd3c7666be82989ab7eca
lancelee2885 <lancelee2885@gmail.com>
2022-02-03 15:11:46 -0800

    wip: adding edit btn if user is authorized

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,78 +1,74 @@
-import styles from '../../styles/Post.module.css';
+import styles from "../../styles/Post.module.css";
+import Link from "next/link";
 import { firestore, getUserWithUsername, postToJson } from "../../lib/firebase";
-import { useDocumentData } from 'react-firebase-hooks/firestore';
+import { useDocumentData } from "react-firebase-hooks/firestore";
 import PostContent from "../../components/PostContent";
-import AuthCheck from '../../components/AuthCheck';
-import HeartBtn from '../../components/HeartBtn';
+import AuthCheck from "../../components/AuthCheck";
+import HeartBtn from "../../components/HeartBtn";
 
 /** ISR - incremental static regeneration
- * This page is statically generated, 
- * BUT regenerated after new requests come in at an interval of 5000ms. 
+ * This page is statically generated,
+ * BUT regenerated after new requests come in at an interval of 5000ms.
  * If a prerendered page does not exist, will fallback to regular SSR.
- * 
+ *
  * Benefits include better performace, fewer db queries, and keeps data current
  */
 
-
 /**
  * tells nextjs to fetch data on the server at built time in order to prerender page in advance
- * 
- * @param param0 { username, slug } 
+ *
+ * @param param0 { username, slug }
  */
 export async function getStaticProps({ params }) {
-    const { username, slug } = params;
-    const userDoc = await getUserWithUsername(username);
+  const { username, slug } = params;
+  const userDoc = await getUserWithUsername(username);
 
-    let post;
-    let path; 
+  let post;
+  let path;
 
-    if (userDoc) {
-        const postRef = userDoc.ref.collection('posts').doc(slug); 
-        post = postToJson(await postRef.get());
-        path = postRef.path;
-    }
+  if (userDoc) {
+    const postRef = userDoc.ref.collection("posts").doc(slug);
+    post = postToJson(await postRef.get());
+    path = postRef.path;
+  }
 
-    return {
-        props: { post, path },
-        revalidate: 5000, // tell next to regen the page when new request come in every 5000 ms
-    }
+  return {
+    props: { post, path },
+    revalidate: 5000, // tell next to regen the page when new request come in every 5000 ms
+  };
 }
 
 /**
  * tells next which path to render
- * @returns 
+ * @returns
  */
 export async function getStaticPaths() {
-    
-    const snapshot = await firestore.collectionGroup('posts').get();
-    const paths = snapshot.docs.map(doc => {
-        const { slug, username } = doc.data();
-        return {
-            params: { username, slug }
-        } 
-    });
-    
-    
+  const snapshot = await firestore.collectionGroup("posts").get();
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
     return {
-        // must be this format:
-        // path = [{
-        //     params: { username, slug}
-        // }]
-        paths,
-        fallback: 'blocking',
-    }
+      params: { username, slug },
+    };
+  });
+
+  return {
+    // must be this format:
+    // path = [{
+    //     params: { username, slug}
+    // }]
+    paths,
+    fallback: "blocking",
+  };
 }
 
 export default function Post(props) {
+  const postRef = firestore.doc(props.path);
+  const [realtimePost] = useDocumentData(postRef);
 
-    const postRef = firestore.doc(props.path);
-    const [realtimePost] = useDocumentData(postRef);
+  const post = realtimePost || props.post;
 
-    const post = realtimePost || props.post;
-
-    return (
+  return (
     <main className={styles.container}>
-
       <section>
         <PostContent post={post} />
       </section>
@@ -81,10 +77,24 @@ export default function Post(props) {
         <p>
           <strong>{post.heartCount || 0} 🤍</strong>
         </p>
-        <AuthCheck>
-          <HeartBtn postRef={postRef}/>
+        <AuthCheck
+          fallback={
+            <>
+              <p> Like this post? Sign up now to give it a heart! </p>
+              <Link href="/enter">
+                <button>Sign up</button>
+              </Link>
+            </>
+          }
+        >
+          <HeartBtn postRef={postRef} />
+          <Link href={`/admin/${post.slug}`}>
+            <h3>
+              <button className="btn-blue">Edit</button>
+            </h3>
+          </Link>
         </AuthCheck>
       </aside>
     </main>
-    );
-  }
+  );
+}
